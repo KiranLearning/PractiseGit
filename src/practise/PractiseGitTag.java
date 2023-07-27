6322f9c4c9bffe8ea41b51acd1cfcb495fc27264
kiran <kiranlearningphase@gmail.com>
2023-07-28 02:54:15 +0530

    adding in v2.0

--- src/practise/PractiseGitTag.java
+++ src/practise/PractiseGitTag.java
@@ -5,7 +5,11 @@ public class PractiseGitTag {
 	public static void main(String[] args) {
 		System.out.println("Adding feature 1");
 		System.out.println("Adding feature 2");
-
+		System.out.println("Adding feature 3");
+		System.out.println("Adding feature 4");
+		
+		
+ 
 	}
 
 }
